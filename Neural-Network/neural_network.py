f4beb541cd32b10a74dc9c7e50a1d7592f281a13
Nisarg Shah <nisargcse@gmail.com>
2020-10-14 15:46:36 -0500

    Forward propogation works. Next up is Backpropogation. Finally need to clean up my code a little bit and arrange files

--- Neural-Network/neural_network.py
+++ Neural-Network/neural_network.py
@@ -22,23 +22,34 @@ def one_hot_encoder(data):
 def initial_weight(units):
     parameters = {}
     L = len(units)
-    for l in range(1, L):
+    for l in range(1,L):
         parameters['W' + str(l)] = np.random.uniform(-0.5,0.5,size=(units[l],units[l-1]))
         parameters['b' + str(l)] = np.zeros((units[l], 1))
-        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
-        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+        assert(parameters['W' + str(l)].shape == (units[l], units[l - 1]))
+        assert(parameters['b' + str(l)].shape == (units[l], 1))
     return parameters
 
 def sigmoid(x):
     return (1 / (1+np.exp(-x)))
 
 def feed_forward(A_prev, W, b):
-    Z = np.dot(W, A_prev) + b
-    assert(Z.shape == (W.shape[0], A_prev.shape[1]))
+    x = A_prev.shape[0]
+    A_prev.reshape(x,1)
+    Z = np.dot(W, A_prev.reshape(x,1)) + b
+    """print(W.transpose().shape)
+    #print(Z)
+    print(Z.shape, A_prev.reshape(x,1).shape[1])"""
+    assert(Z.shape == (W.shape[0], A_prev.reshape(x,1).shape[1]))
     cache = (A_prev, W, b)
-    return Z, cache, sigmoid(Z)
+    return (Z, cache), sigmoid(Z)
 
+def compute_loss(computed, original):
+    loss = 0
+    for i in range(computed.shape[0]):
+        loss += np.square(computed[i]-original[i])
 
+    #print(loss)
+    return loss
 
 
 def neural_network(train_file, test_file, layers, units_per_layer, rounds):
@@ -55,18 +66,42 @@ def neural_network(train_file, test_file, layers, units_per_layer, rounds):
     ##Normalizign the data on X_train and X_test
     X_train = X_train/X_train.max()
     X_test = X_test/X_test.max()
+    vector_train = one_hot_encoder(y_train.astype(np.int))
+    vector_test = one_hot_encoder(y_test.astype(np.int))
+    print(vector_train[0].shape)
 
     units = []
 
     #print(units)
     units = [units_per_layer for i in range(1,layers+1)]
 
-    units.insert(0,X_train.shape[0])
+    units.insert(0,X_train.shape[1])
     numOfClasses = np.max(y_train) - np.min(y_test) +1
     units.append(int(numOfClasses))
-    print(units)
-    #print(X_test.shape)
+    #print(units)
+    parameters = initial_weight(units)
 
+    #print(X_test.shape)
+    cache= []
+    loss = 0
+    for j in range(len(X_train)):
+        caches_per_layer = []
+        A = X_train[j]
+        for i in range(1,len(units)):
+            #print(i)
+            A_prev = A
+            #print(A_prev.shape, parameters['W' + str(i)].shape )
+            cache, A = feed_forward(A_prev,parameters['W' + str(i)],parameters['b' + str(i)])
+            caches_per_layer.append(cache)
+            #print(j)
+        #cache.append(caches_per_layer)
+
+        print(A.shape, vector_train[j].shape)
+        loss += compute_loss(A,vector_train[j])
+    #print(caches_per_layer)
+
+    #print(X_train[0].shape)
+    #print(loss)
 
 
 
