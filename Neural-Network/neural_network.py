b3bb3d007bd9714604e1189dc79e2ce9e54024d4
Nisarg Shah <nisargcse@gmail.com>
2020-10-14 07:41:46 -0500

    Redoing some of the functions as they weren't working. Going on a totally different route

--- Neural-Network/neural_network.py
+++ Neural-Network/neural_network.py
@@ -1,105 +1,35 @@
-import numpy as np
 import sys
+import os
+import numpy as np
 
+def extract_data(file):
+    data = np.loadtxt(file)
+    class_data = data[:,-1]
+    attr_data = data[:,:-1]
+    return attr_data,class_data
 
-'''
-This function will be used to return sigmoid
-
-
-'''
-def sigmoid(x):
-    return 1 / (1+np.exp(-x))
-
-def initial_weight(rows, columns):
-    w = np.random.uniform(-0.5,0.5,(rows, columns))
-    b = np.zeros((columns,1))
-    return w,b
-
-
-
-def one_hot_encoder(a):
-    if a.min() == 0:
-        b = np.zeros((a.size, a.max()+1))
-        b[np.arange(a.size),a] = 1
-        return b
+def one_hot_encoder(data):
+    if data.min() == 0:
+        vector = np.zeros((data.size, data.max()+1))
+        vector[np.arange(data.size),data] = 1
+        return vector
     else:
-        b = np.zeros((a.size, a.max()))
-        b[np.arange(a.size),a-1] = 1
-        return b
-
-def feed_forward(W,X,b):
-
-    Z = np.dot(W,X) + b
-    A = sigmoid(Z)
-    return A
-
-def cost(t,Z):
-    assert t.shape == Z.shape
-    return (1 / 2)*np.sum(np.square(t-Z))
-
+        vector = np.zeros((data.size, data.max()))
+        vector[np.arange(data.size),data-1] = 1
+        return vector
 
 
 def neural_network(train_file, test_file, layers, units_per_layer, rounds):
-    ##First step is to read in the files.
 
-    train_data = np.loadtxt(train_file)
-    #print(train_data)
-    test_data = np.loadtxt(test_file)
-    #print(test_data)
-    X_train = train_data[:,:-1]
-    y_train = train_data[:,-1].astype(int)
-    X_test = test_data[:,:-1]
-    y_test = test_data[:,-1]
+    ##Read in the files
+    X_train, y_train = extract_data(train_file)
+    X_test, y_test = extract_data(test_file)
+    #print(X_train)
 
-
-    max1 = max(X_train.min(), X_train.max(), key=abs)
-    X_train = X_train/max1
-    X_test = X_test/max1
-
-    ##First we will check if we have correct inputs:
-    if int(layers) < 2:
-        print("Please enter correct number of input layers:")
-        return 0
-    one_hot_y = one_hot_encoder(y_train)
-    x = X_train[0]
-    x = x.reshape(X_train.shape[1],1)
-    #print(x.shape)
-
-    w,b = initial_weight(X_train.shape[1], 1)
-    z = feed_forward(w,x.transpose(),b)
-    for i in range(1,layers):
-
-        if i == layers-1:
-            w,b = initial_weight(len(np.unique(y_train)),1)
-        else:
-
-            w,b = initial_weight(units_per_layer,z.shape[1])
-        z = feed_forward(w,z.transpose(),b)
-        print(w.shape,z.shape)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ##Converting our class_data to one hot encoder
+    print(y_test)
+    print(one_hot_encoder(y_test.astype(np.int))[0])
 
 
 if __name__ == '__main__':
-    x = neural_network(sys.argv[1],sys.argv[2],int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]))
-
-
-
-
-# neural_network(<training_file>, <test_file>, <layers>, <units_per_layer>, <rounds>)
+    neural_network(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
