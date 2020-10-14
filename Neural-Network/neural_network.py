36a51bc50c25c983a8b68a7cadf5339a6c47c7cc
Nisarg Shah <nisargcse@gmail.com>
2020-10-14 08:21:56 -0500

    One hot vector done, normalization of input done, got units per layers which will help in the dimentions of weights. Next step to initiaze the weights and designing a feed_forward and backpropogation network

--- Neural-Network/neural_network.py
+++ Neural-Network/neural_network.py
@@ -19,6 +19,7 @@ def one_hot_encoder(data):
         return vector
 
 
+
 def neural_network(train_file, test_file, layers, units_per_layer, rounds):
 
     ##Read in the files
@@ -27,9 +28,23 @@ def neural_network(train_file, test_file, layers, units_per_layer, rounds):
     #print(X_train)
 
     ##Converting our class_data to one hot encoder
-    print(y_test)
-    print(one_hot_encoder(y_test.astype(np.int))[0])
+    """print(y_test)
+    print(one_hot_encoder(y_test.astype(np.int))[0])"""
 
+    ##Normalizign the data on X_train and X_test
+    X_train = X_train/X_train.max()
+    X_test = X_test/X_test.max()
+
+    units = []
+
+    #print(units)
+    units = [units_per_layer for i in range(1,layers+1)]
+
+    units.insert(0,X_train.shape[0])
+    numOfClasses = np.max(y_train) - np.min(y_test) +1
+    units.append(int(numOfClasses))
+    print(units)
+    #print(X_test.shape)
 
 if __name__ == '__main__':
     neural_network(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
