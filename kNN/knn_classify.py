6059f51ea7dfc0e62e8ab6b4e79d4de85da7a987
nisargushah <58926548+nisargushah@users.noreply.github.com>
2020-11-10 15:43:02 -0600

    Update knn_classify.py
    Optimized one for loop

--- kNN/knn_classify.py
+++ kNN/knn_classify.py
@@ -4,17 +4,10 @@ from collections import Counter
 from statistics import stdev
 
 def normalise(data, test_data):
-    data = np.asarray(data)
-    #test_data = np.asarray(test_data, dtype=np.float32)
-
-    stats = [[np.mean(data[:,i]), np.std(data[:,i])] for i in range(0,data.shape[1]-1)]
-    #print(stats)
-    for x in range(0, data.shape[0]):
-        for y in range(0,data.shape[1]-1):
-            data[x][y] = (data[x][y] - stats[y][0])/stats[y][1]
-    for x in range(0, test_data.shape[0]):
-        for y in range(0,test_data.shape[1]-1):
-            test_data[x][y] = (test_data[x][y] - stats[y][0])/stats[y][1]
+    max = np.max(data[:,:-1])
+    data[:,:-1] = data[:,:-1]/max
+    max = np.max(test_data[:,:-1])
+    test_data[:,:-1] = test_data[:,:-1]/max
     return data, test_data
 
 def nearest_neighbor(train_data, test_data, k):
@@ -28,7 +21,7 @@ def nearest_neighbor(train_data, test_data, k):
         votes = [i[1] for i in sorted(distance)[:k]]
         predicted_class = Counter(votes).most_common(1)[0][0]
         votes = np.asarray(votes)
-        print(object_id, votes)
+        #print(object_id, votes)
         if len(np.unique(votes)) == k:
             if predict[-1] in votes:
                 accuracy.append(1/k)
@@ -36,7 +29,7 @@ def nearest_neighbor(train_data, test_data, k):
                 accuracy.append(0)
         else:
             accuracy.append(1) if predicted_class == predict[-1] else accuracy.append(0)
-        #print('ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f'% (object_id, predicted_class, predict[-1], accuracy[-1]));
+        print('ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f'% (object_id, predicted_class, predict[-1], accuracy[-1]));
         object_id += 1
     print('classification accuracy=%6.2f'%(sum(accuracy)/len(accuracy)*100))
 
